29e36d3a40754bb71617ceff1828bafa8869d8c7
Atsushi Nagase <a@ngs.io>
2018-07-08 01:27:53 +0900

    Fix spec

--- spec/coveralls_action_spec.rb
+++ spec/coveralls_action_spec.rb
@@ -1,19 +1,23 @@
 require 'xccoveralls/runner'
+require 'xccoveralls/options'
 
 describe Fastlane::Actions::CoverallsAction do
   describe '#run' do
     it 'Runs command' do
       params = {
         repo_token: 'asdf',
-        derived_data_path: "your/custom/DerivedData",
-        source_path: "your/src",
-        ignorefile_path: "your/src/.coverallsignore"
+        derived_data_path: ".",
+        source_path: ".",
+        ignorefile_path: nil
       }
+      expect(FastlaneCore::Helper).to receive(:mac?).and_return(true)
+      opts = described_class.available_options
+      config = FastlaneCore::Configuration.create(opts, params)
       fake_runner = 'runner'
       expect(fake_runner).to receive(:run!)
       expect(Xccoveralls::Runner).to receive(:new).with(params).and_return(fake_runner)
 
-      Fastlane::Actions::CoverallsAction.run(params)
+      Fastlane::Actions::CoverallsAction.run(config)
     end
   end
 end
